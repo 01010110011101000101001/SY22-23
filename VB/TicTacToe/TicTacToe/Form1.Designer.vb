5392803767a4cb6fbb5a2ae26e51ae87dc8ddc45
Clara <cabai999@learner.dvusd.org>
2022-09-27 14:05:17 -0700

    message
    si

---
+++ VB/TicTacToe/TicTacToe/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.b1 = New System.Windows.Forms.Button()
+        Me.b2 = New System.Windows.Forms.Button()
+        Me.b3 = New System.Windows.Forms.Button()
+        Me.b4 = New System.Windows.Forms.Button()
+        Me.b5 = New System.Windows.Forms.Button()
+        Me.b6 = New System.Windows.Forms.Button()
+        Me.b7 = New System.Windows.Forms.Button()
+        Me.b8 = New System.Windows.Forms.Button()
+        Me.b9 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.p1score = New System.Windows.Forms.Label()
+        Me.p2score = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'b1
+        '
+        Me.b1.Location = New System.Drawing.Point(268, 113)
+        Me.b1.Name = "b1"
+        Me.b1.Size = New System.Drawing.Size(75, 70)
+        Me.b1.TabIndex = 0
+        Me.b1.Text = " "
+        Me.b1.UseVisualStyleBackColor = True
+        '
+        'b2
+        '
+        Me.b2.Location = New System.Drawing.Point(349, 113)
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(75, 70)
+        Me.b2.TabIndex = 1
+        Me.b2.Text = " "
+        Me.b2.UseVisualStyleBackColor = True
+        '
+        'b3
+        '
+        Me.b3.Location = New System.Drawing.Point(430, 113)
+        Me.b3.Name = "b3"
+        Me.b3.Size = New System.Drawing.Size(75, 70)
+        Me.b3.TabIndex = 2
+        Me.b3.Text = " "
+        Me.b3.UseVisualStyleBackColor = True
+        '
+        'b4
+        '
+        Me.b4.Location = New System.Drawing.Point(268, 189)
+        Me.b4.Name = "b4"
+        Me.b4.Size = New System.Drawing.Size(75, 70)
+        Me.b4.TabIndex = 3
+        Me.b4.Text = " "
+        Me.b4.UseVisualStyleBackColor = True
+        '
+        'b5
+        '
+        Me.b5.Location = New System.Drawing.Point(349, 189)
+        Me.b5.Name = "b5"
+        Me.b5.Size = New System.Drawing.Size(75, 70)
+        Me.b5.TabIndex = 4
+        Me.b5.Text = " "
+        Me.b5.UseVisualStyleBackColor = True
+        '
+        'b6
+        '
+        Me.b6.Location = New System.Drawing.Point(430, 189)
+        Me.b6.Name = "b6"
+        Me.b6.Size = New System.Drawing.Size(75, 70)
+        Me.b6.TabIndex = 5
+        Me.b6.Text = " "
+        Me.b6.UseVisualStyleBackColor = True
+        '
+        'b7
+        '
+        Me.b7.Location = New System.Drawing.Point(268, 265)
+        Me.b7.Name = "b7"
+        Me.b7.Size = New System.Drawing.Size(75, 70)
+        Me.b7.TabIndex = 6
+        Me.b7.Text = " "
+        Me.b7.UseVisualStyleBackColor = True
+        '
+        'b8
+        '
+        Me.b8.Location = New System.Drawing.Point(349, 265)
+        Me.b8.Name = "b8"
+        Me.b8.Size = New System.Drawing.Size(75, 70)
+        Me.b8.TabIndex = 7
+        Me.b8.Text = " "
+        Me.b8.UseVisualStyleBackColor = True
+        '
+        'b9
+        '
+        Me.b9.Location = New System.Drawing.Point(430, 265)
+        Me.b9.Name = "b9"
+        Me.b9.Size = New System.Drawing.Size(75, 70)
+        Me.b9.TabIndex = 8
+        Me.b9.Text = " "
+        Me.b9.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(349, 389)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Reset"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 16)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Player 1:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 113)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 16)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Player 2:"
+        '
+        'p1score
+        '
+        Me.p1score.AutoSize = True
+        Me.p1score.Location = New System.Drawing.Point(75, 54)
+        Me.p1score.Name = "p1score"
+        Me.p1score.Size = New System.Drawing.Size(14, 16)
+        Me.p1score.TabIndex = 12
+        Me.p1score.Text = "0"
+        '
+        'p2score
+        '
+        Me.p2score.AutoSize = True
+        Me.p2score.Location = New System.Drawing.Point(75, 113)
+        Me.p2score.Name = "p2score"
+        Me.p2score.Size = New System.Drawing.Size(14, 16)
+        Me.p2score.TabIndex = 13
+        Me.p2score.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.p2score)
+        Me.Controls.Add(Me.p1score)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.b9)
+        Me.Controls.Add(Me.b8)
+        Me.Controls.Add(Me.b7)
+        Me.Controls.Add(Me.b6)
+        Me.Controls.Add(Me.b5)
+        Me.Controls.Add(Me.b4)
+        Me.Controls.Add(Me.b3)
+        Me.Controls.Add(Me.b2)
+        Me.Controls.Add(Me.b1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents b1 As Button
+    Friend WithEvents b2 As Button
+    Friend WithEvents b3 As Button
+    Friend WithEvents b4 As Button
+    Friend WithEvents b5 As Button
+    Friend WithEvents b6 As Button
+    Friend WithEvents b7 As Button
+    Friend WithEvents b8 As Button
+    Friend WithEvents b9 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents p1score As Label
+    Friend WithEvents p2score As Label
+End Class
---
+++ VB/TicTacToe/TicTacToe/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Dim turn As String = "X"
+    Dim score1 As Integer
+    Dim score2 As Integer
+
+    Sub reset()
+        Me.BackColor = Color.White
+        b1.Text = ""
+        b2.Text = ""
+        b3.Text = ""
+        b4.Text = ""
+        b5.Text = ""
+        b6.Text = ""
+        b7.Text = ""
+        b8.Text = ""
+        b9.Text = ""
+        p1score.Text = score1
+        p2score.Text = score2
+    End Sub
+
+    Private Sub b1_Click(sender As Object, e As EventArgs)
+        pick(sender)
+    End Sub
+    Sub pick(b As Button)
+        b.Text = turn
+        If turn = "X" Then
+            turn = "O"
+        Else
+            turn = "X"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        reset()
+    End Sub
+
+    Private Sub b2_Click_1(sender As Object, e As EventArgs) Handles b9.Click, b8.Click, b7.Click, b6.Click, b5.Click, b4.Click, b3.Click, b2.Click, b1.Click
+        pick(sender)
+        If Iswinner() Then
+            Me.BackColor = Color.Green
+            If turn = "O" Then
+                score1 = score1 + 1
+            Else
+                score2 = score2 + 1
+            End If
+            p1score.Text = score1
+            p2score.Text = score2
+            MsgBox("winner")
+            reset()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        reset()
+    End Sub
+
+    Function Iswinner() As Boolean
+        If b1.Text = b2.Text And b2.Text = b3.Text And b1.Text <> "" Then Return True
+        If b1.Text = b4.Text And b4.Text = b7.Text And b1.Text <> "" Then Return True
+        If b1.Text = b5.Text And b5.Text = b9.Text And b1.Text <> "" Then Return True
+        If b2.Text = b5.Text And b5.Text = b8.Text And b2.Text <> "" Then Return True
+        If b3.Text = b6.Text And b6.Text = b9.Text And b3.Text <> "" Then Return True
+        If b7.Text = b8.Text And b8.Text = b9.Text And b7.Text <> "" Then Return True
+        If b3.Text = b5.Text And b5.Text = b7.Text And b3.Text <> "" Then Return True
+        If b4.Text = b5.Text And b5.Text = b6.Text And b4.Text <> "" Then Return True
+
+        Return False
+    End Function
+End Class
